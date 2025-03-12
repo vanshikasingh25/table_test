f437fa4cebf25567d26e9198dea5451c54e431ff
Dewanshu Singh <111553613+Devildev07@users.noreply.github.com>
2025-03-12 19:50:43 +0000

    prime ng provider issue fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    providePrimeNG({
+      theme: {
+          preset: Aura
+      }
+  })
+  ],
 };
